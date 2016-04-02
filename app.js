fbb8c3fb67f2833fcb73d71450f0294e67dc5ecb
Aleah Ellis <aleah.is@icloud.com>
2016-04-02 10:06:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var grocery=[
+	
+	{
+		name:"Chocolate Chips" ,
+		price:3
+	},
+	{
+		name:"Bacon",
+		price:4
+	},
+	{
+		name:"Salad Things",
+		price:12
+	},
+	{
+		name:"Ice Cream",
+		price:5
+	},
+	{
+		name:"Almonds",
+		price:12
+	},
+	{
+		name:"Raspberries",
+		price:2
+	}];
+
+
+var total=0;
+
+
+grocery.forEach(function(items){ //loop through array
+		total += items.price; //
+		var listEle = document.createElement('div'); //makes the div
+		listEle.innerHTML = items.name + "= " + "$" + items.price;//			
+		document.body.appendChild(listEle);
+		listEle.setAttribute("class", "bombae");
+		});
+		
+
+//putting everything on the html... Dom Manipulation
+var totalEle = document.createElement('div'); //makes the div
+totalEle.innerHTML = "Total = " + "$" + total; //total goes to the html
+document.body.appendChild(totalEle); //appending the price total to the div
+totalEle.setAttribute("class", "fancy");//setting style attributes to the div
+
+
+
